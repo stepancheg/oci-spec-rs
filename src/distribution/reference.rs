3d36add40af431bc926ad265794c8e80b231e13d
Stepan Koltsov <stepan.koltsov@gmail.com>
2025-04-29 21:49:53 +0100

    Unnecessary string copy in TryFrom<&str> for Reference
    Signed-off-by: Stepan Koltsov <st@x.ai>

--- src/distribution/reference.rs
+++ src/distribution/reference.rs
@@ -225,10 +225,10 @@ impl FromStr for Reference {
     }
 }
 
-impl TryFrom<String> for Reference {
+impl TryFrom<&str> for Reference {
     type Error = ParseError;
 
-    fn try_from(s: String) -> Result<Self, Self::Error> {
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
         if s.is_empty() {
             return Err(ParseError::NameEmpty);
         }
@@ -282,10 +282,10 @@ impl TryFrom<String> for Reference {
     }
 }
 
-impl TryFrom<&str> for Reference {
+impl TryFrom<String> for Reference {
     type Error = ParseError;
-    fn try_from(string: &str) -> Result<Self, Self::Error> {
-        TryFrom::try_from(string.to_owned())
+    fn try_from(string: String) -> Result<Self, Self::Error> {
+        TryFrom::try_from(string.as_str())
     }
 }
 
